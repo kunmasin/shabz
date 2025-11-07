d04f71c6471ebf7213577be572d9677b301d141b
Kunmasin <oniyeabdullahi00@gmail.com>
2025-11-07 09:49:55 +0100

    ''New One"

--- shaaba_car_rentals.sql
+++ shaaba_car_rentals.sql
@@ -1,54 +1,194 @@
-CREATE TABLE IF NOT EXISTS users(
-	id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    fullName VARCHAR(100),
-    usernames VARCHAR(40),
-    passwords VARCHAR(15),
-    email VARCHAR(60),
-    address VARCHAR(255),
-    phoneNumber VARCHAR(12),
-    images VARCHAR(255),
-    date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 07, 2025 at 09:49 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-CREATE TABLE IF NOT EXISTS regiteration(
-	id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    fullName VARCHAR(100),
-    usernames VARCHAR(40),
-    passwords VARCHAR(15),
-    email VARCHAR(60),
-    address VARCHAR(255),
-    phoneNumber VARCHAR(12),
-    images VARCHAR(255),
-    date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS cars(
-	 id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    car_name VARCHAR(50),
-    car_color VARCHAR(30),
-    car_status VARCHAR(40),
-    lease_price DECIMAL(20),
-    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS drivers(
-	id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    driver_name VARCHAR(100),
-    driver_email VARCHAR(60),
-    driver_address VARCHAR(255),
-    driver_number VARCHAR(12),
-    driver_images VARCHAR(255),
-    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE IF NOT EXISTS bookings(
-	id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(100),
-    car_destination VARCHAR(255),
-    car_name VARCHAR(50),
-    car_color VARCHAR(12),
-    nin VARCHAR(40),
-    starting_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ending_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shaaba_car_rentals`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(100) DEFAULT NULL,
+  `car_destination` varchar(255) DEFAULT NULL,
+  `car_name` varchar(50) DEFAULT NULL,
+  `car_color` varchar(12) DEFAULT NULL,
+  `nin` varchar(40) DEFAULT NULL,
+  `starting_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `ending_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
+CREATE TABLE `cars` (
+  `id` int(11) NOT NULL,
+  `car_name` varchar(50) DEFAULT NULL,
+  `car_color` varchar(30) DEFAULT NULL,
+  `car_status` varchar(40) DEFAULT NULL,
+  `lease_price` decimal(20,0) DEFAULT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drivers`
+--
+
+CREATE TABLE `drivers` (
+  `id` int(11) NOT NULL,
+  `driver_name` varchar(100) DEFAULT NULL,
+  `driver_email` varchar(60) DEFAULT NULL,
+  `driver_address` varchar(255) DEFAULT NULL,
+  `driver_number` varchar(12) DEFAULT NULL,
+  `driver_images` varchar(255) DEFAULT NULL,
+  `date_joined` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registeration`
+--
+
+CREATE TABLE `registeration` (
+  `id` int(11) NOT NULL,
+  `fullName` varchar(100) DEFAULT NULL,
+  `usernames` varchar(40) DEFAULT NULL,
+  `passwords` varchar(15) DEFAULT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phoneNumber` varchar(12) DEFAULT NULL,
+  `images` varchar(255) DEFAULT NULL,
+  `date_joined` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `registeration`
+--
+
+INSERT INTO `registeration` (`id`, `fullName`, `usernames`, `passwords`, `email`, `address`, `phoneNumber`, `images`, `date_joined`) VALUES
+(1, '', '', '', '', '', '', '', '2025-11-05 11:34:06'),
+(2, '', 'Young Shabz', 'shabz001', 'faruqmuhammed3333@gmail.com', 'Emi Shaaba, Patigi, Kwara State, Nigeria.', '08033850768', 'images/1762342503_images (1).jpeg', '2025-11-05 11:35:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fullName` varchar(100) DEFAULT NULL,
+  `usernames` varchar(40) DEFAULT NULL,
+  `passwords` varchar(15) DEFAULT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phoneNumber` varchar(12) DEFAULT NULL,
+  `images` varchar(255) DEFAULT NULL,
+  `date_joined` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullName`, `usernames`, `passwords`, `email`, `address`, `phoneNumber`, `images`, `date_joined`) VALUES
+(2, 'Abdullahi Masud Oniye', 'Kunmasin', 'kunmasin', 'kunmasin@gmail.com', 'No 56 Makama, Villa, Eruda, Ilorin, Kwara State.', '09015621510', 'users_image/1762339792_His Her Wedding Rings Set Trio Men Women 10k Yellow Gold Real Diamonds.jpeg', '2025-11-05 10:49:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `drivers`
+--
+ALTER TABLE `drivers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registeration`
+--
+ALTER TABLE `registeration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `drivers`
+--
+ALTER TABLE `drivers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registeration`
+--
+ALTER TABLE `registeration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
